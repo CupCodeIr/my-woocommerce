719452abf03396afce3f6aaf6a56753d11cb50b8
Artin <info@cupcode.xyz>
2021-09-13 23:25:00 +0430

    Changes related to Attribute.php changes

--- includes/class/SelectableAttribute.php
+++ includes/class/SelectableAttribute.php
@@ -10,12 +10,11 @@ class SelectableAttribute extends Attribute
 {
 
     private static $instance;
-    protected $wpdb;
 
 
     private function __construct()
     {
-
+        parent::__construct();
     }
 
 
@@ -28,8 +27,6 @@ class SelectableAttribute extends Attribute
     {
         if (self::$instance === null) {
             self::$instance = new self();
-            global $wpdb;
-            self::$instance->wpdb = $wpdb;
             self::$instance->init();
 
         }
@@ -39,12 +36,15 @@ class SelectableAttribute extends Attribute
 
     protected function init()
     {
+        parent::init();
+
         add_action('init', function () {
 
             $this->register_attribute_post_type();
-            add_action('save_post_'. CC_MYWC_PLUGIN_SLUG . '_sa', [$this, 'save_attributes']);
-            add_filter('redirect_post_location', [$this,'maybe_redirect_for_error_display']);
-            add_filter( 'post_updated_messages',[$this, 'post_save_message'] );
+            add_action('save_post_' . CC_MYWC_PLUGIN_SLUG . '_sa', [$this, 'save_attributes']);
+            add_filter('redirect_post_location', [$this, 'maybe_redirect_for_error_display']);
+            add_filter('post_updated_messages', [$this, 'post_type_save_message']);
+            add_filter('admin_notices', [$this, 'post_type_save_error_display']);
 
         });
     }
@@ -55,7 +55,7 @@ class SelectableAttribute extends Attribute
      * @since 0.1.0
      */
 
-    public function register_attribute_post_type() : bool
+    public function register_attribute_post_type(): bool
     {
 
         $labels = [
@@ -80,11 +80,11 @@ class SelectableAttribute extends Attribute
             'filter_items_list' => esc_html_x('Filter selectable attribute list', 'post-type', 'cupcode-mywc'),
             'items_list_navigation' => esc_html_x('Selectable Attributes list navigation', 'post-type', 'cupcode-mywc'),
             'items_list' => esc_html_x('Selectable Attributes list', 'post-type', 'cupcode-mywc'),
-            'item_published'           => esc_html_x( 'Selectable Attribute published.','post-type', 'cupcode-mywc' ),
-            'item_published_privately' => esc_html_x( 'Selectable Attribute published privately.','post-type', 'cupcode-mywc' ),
-            'item_reverted_to_draft'   => esc_html_x( 'Selectable Attribute reverted to draft.','post-type', 'cupcode-mywc' ),
-            'item_scheduled'           => esc_html_x( 'Selectable Attribute scheduled.','post-type', 'cupcode-mywc' ),
-            'item_updated'             => esc_html_x( 'Selectable Attribute updated.','post-type', 'cupcode-mywc' ),
+            'item_published' => esc_html_x('Selectable Attribute published.', 'post-type', 'cupcode-mywc'),
+            'item_published_privately' => esc_html_x('Selectable Attribute published privately.', 'post-type', 'cupcode-mywc'),
+            'item_reverted_to_draft' => esc_html_x('Selectable Attribute reverted to draft.', 'post-type', 'cupcode-mywc'),
+            'item_scheduled' => esc_html_x('Selectable Attribute scheduled.', 'post-type', 'cupcode-mywc'),
+            'item_updated' => esc_html_x('Selectable Attribute updated.', 'post-type', 'cupcode-mywc'),
         ];
 
         $args = [
@@ -140,15 +140,15 @@ class SelectableAttribute extends Attribute
         $attribute_taxonomies = wc_get_attribute_taxonomies();
         $wc_excluded_terms = $this->get_used_taxonomies();
         $wc_remained_taxonomies = get_terms([
-            'taxonomy'   => ['product_cat','product_tag'],
+            'taxonomy' => ['product_cat', 'product_tag'],
             'hide_empty' => true,
             'count' => false,
-            'exclude'    => $wc_excluded_terms
+            'exclude' => $wc_excluded_terms
         ]);
         $wc_remained_categories = [];
         $wc_remained_tags = [];
-        foreach ($wc_remained_taxonomies as $remained_taxonomy){
-            if($remained_taxonomy->taxonomy === "product_cat")
+        foreach ($wc_remained_taxonomies as $remained_taxonomy) {
+            if ($remained_taxonomy->taxonomy === "product_cat")
                 $wc_remained_categories[] = $remained_taxonomy;
             elseif ($remained_taxonomy->taxonomy === "product_tag")
                 $wc_remained_tags[] = $remained_taxonomy;
@@ -157,9 +157,10 @@ class SelectableAttribute extends Attribute
             [
                 'locale' => $locale,
                 'attribute_taxonomies' => $attribute_taxonomies,
+                'selected_data' => $this->get_post_attribute_meta($post->ID),
                 'categories' => $wc_remained_categories,
                 'post_id' => $post->ID,
-                'categories_map' => wp_list_pluck($wc_remained_categories,'name', 'term_id'),
+                'categories_map' => wp_list_pluck($wc_remained_categories, 'name', 'term_id'),
                 'tags' => $wc_remained_tags
             ]
         );
@@ -171,18 +172,16 @@ class SelectableAttribute extends Attribute
      * @return array
      * @since  0.1.0
      */
-    public final function post_save_message(array $messages) : array {
+    public final function post_type_save_message(array $messages): array
+    {
 
-        $error = isset($_GET[CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error']) ? $_GET[CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error'] : false;
-        if($error !== false && $error === "no_cat_tag_passed"){
-            $error_message = esc_html__('Please, at least select one product category or product tag.', 'cupcode-mywc');
-            $messages['post'][1] = $error_message;
-            $messages['post'][4] = $error_message;
-            $messages['post'][6] = $error_message;
-            $messages['post'][7] = $error_message;
-            $messages['post'][9] = $error_message;
-            $messages['post'][10] = $error_message;
-        }
+
+        $messages['post'][1] = esc_html__('Selectable Attribute updated.', 'cupcode-mywc');
+        $messages['post'][4] = esc_html__('Selectable Attribute updated.', 'cupcode-mywc');
+        $messages['post'][6] = esc_html__('Selectable Attribute published.', 'cupcode-mywc');
+        $messages['post'][9] = esc_html__('Selectable Attribute updated.', 'cupcode-mywc');
+        $messages['post'][10] = esc_html__('Selectable Attribute draft updated.', 'cupcode-mywc');
+
         return $messages;
 
 
@@ -197,9 +196,28 @@ class SelectableAttribute extends Attribute
     public final function maybe_redirect_for_error_display($location): string
     {
         $error = isset($_POST[CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error']) ? $_POST[CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error'] : false;
-        if($error !== false)
+        if ($error !== false)
             $location .= "&" . CC_MYWC_PLUGIN_SLUG . "_admin_attr_save_error={$error}";
         return $location;
+    }
+
+    /**
+     *
+     */
+    public final function post_type_save_error_display()
+    {
+
+        if (isset($_GET[CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error'])) {
+
+            $html = '<div class="notice notice-error is-dismissible">';
+            $error_code = $_GET[CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error'];
+            if ($error_code === "no_cat_tag_passed") {
+                $html .= '<p><strong>' . esc_html__('Please select at least a product category or a product tag.', 'cupcode-mywc') . '</strong></p>';
+            }
+            $html .= '</div>';
+            echo $html;
+        }
+
     }
 
     /**
@@ -208,7 +226,7 @@ class SelectableAttribute extends Attribute
      * @return bool
      * @since 0.1.0
      */
-    public function save_attributes(int $post_id) : bool
+    public function save_attributes(int $post_id): bool
     {
 
         $nonce_name = isset($_POST[CC_MYWC_PLUGIN_SLUG . '_save_selectable_attributes_nonce']) ? $_POST[CC_MYWC_PLUGIN_SLUG . '_save_selectable_attributes_nonce'] : '';
@@ -237,15 +255,30 @@ class SelectableAttribute extends Attribute
         $tags = $_POST['cupcode_mywc-tags'];
         $attributes = $_POST['cupcode_mywc-attributes'];
 
-        if((count($categories) < 1 && count($tags) < 1) || count($attributes) < 1){
-            $_POST[CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error' ] = 'no_cat_tag_passed';
+        if ((count($categories) < 1 && count($tags) < 1) || count($attributes) < 1) {
+            $_POST[CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error'] = 'no_cat_tag_passed';
             return false;
         }
+        $insert_query = "insert into {$this->wpdb->postmeta} (post_id,meta_key,meta_value) values ";
+        $insert_values = [];
+        foreach ($categories as $category){
+            $insert_values[] = $this->wpdb->prepare("(%d,%s,%s)",$post_id,'_' . CC_MYWC_PLUGIN_SLUG . '_category',$category);
+        }
+        foreach ($tags as $tag){
+            $insert_values[] = $this->wpdb->prepare("(%d,%s,%s)",$post_id,'_' . CC_MYWC_PLUGIN_SLUG . '_tag',$tag);
+        }
+        foreach ($attributes as $attribute){
+            $insert_values[] = $this->wpdb->prepare("(%d,%s,%s)",$post_id,'_' . CC_MYWC_PLUGIN_SLUG . '_attribute',$attribute);
+        }
+        $insert_query .= implode(",\n",$insert_values);
 
-
-
-        return true;
-
+        $delete_query = "delete from {$this->wpdb->postmeta} where post_id = {$post_id} and 
+                        (meta_key = '_" . CC_MYWC_PLUGIN_SLUG . "_category' or
+                        meta_key = '_" . CC_MYWC_PLUGIN_SLUG . "_tag' or
+                        meta_key = '_" . CC_MYWC_PLUGIN_SLUG . "_attribute')";
+        if($this->wpdb->query($delete_query) !== false)
+            return ($this->wpdb->query($insert_query) === (count($categories) + count($tags) + count($attributes) ));
+        return false;
 
 
 
