5da0acd1288bba7be19d6b1856651e79b07adfb5
Artin <info@cupcode.xyz>
2021-09-13 23:26:11 +0430

    admin notice removed. selected attributes will be shown now

--- includes/template/admin-attribute-edit-page-meta-box.php
+++ includes/template/admin-attribute-edit-page-meta-box.php
@@ -7,19 +7,7 @@
 
 defined('ABSPATH') or die('No script kiddies please!');
 
-$error = get_transient(CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error');
 ?>
-<?php if ($error !== false) { ?>
-    <div class="notice notice-error is-dismissible">
-        <?php if ($error['error_code'] === "no_cat_tag_passed" && $post_id === $error['post_id']) { ?>
-            <p>
-                <strong><?php esc_html_e('Please, at least select one product category or product tag.', 'cupcode-mywc'); ?></strong>
-            </p>
-        <?php } ?>
-    </div>
-    <?php
-    delete_transient(CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error');
-} ?>
 
     <p><strong><?php esc_html_e('Selectable Product Categories', 'cupcode-mywc'); ?></strong></p>
     <p><?php esc_html_e('Please select the WooCommerce Product Categories that your customers can choose to define their desired Attributes in.', 'cupcode-mywc') ?></p>
@@ -27,7 +15,7 @@ $error = get_transient(CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error');
             dir="<?php echo(is_rtl() ? 'rtl' : 'ltr') ?>" lang="<?php echo $locale ?>"
             name="<?php echo CC_MYWC_PLUGIN_SLUG ?>-categories[]">';
         <?php foreach ($categories as $category) { ?>
-            <option value="<?php echo $category->term_id ?>"><?php
+            <option <?php echo (in_array($category->term_id,$selected_data['categories'])) ? 'selected="selected"' : '' ?> value="<?php echo $category->term_id ?>"><?php
                 echo $category->name . ' ' . (($category->parent !== 0) ? sprintf(esc_html__('(Parent Category: %s)', 'cupcode-mywc'), $categories_map[$category->parent]) : '')
                 ?></option>
         <?php } ?>
@@ -41,7 +29,7 @@ $error = get_transient(CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error');
             dir="<?php echo(is_rtl() ? 'rtl' : 'ltr') ?>" lang="<?php echo $locale ?>"
             name="<?php echo CC_MYWC_PLUGIN_SLUG ?>-tags[]">';
         <?php foreach ($tags as $tag) { ?>
-            <option value="<?php echo $tag->term_id ?>"><?php echo $tag->name ?></option>
+            <option <?php echo (in_array($tag->term_id,$selected_data['tags'])) ? 'selected="selected"' : '' ?> value="<?php echo $tag->term_id ?>"><?php echo $tag->name ?></option>
         <?php } ?>
     </select>
     <small><?php esc_html_e('Empty tags are excluded.', 'cupcode-mywc'); ?></small>
@@ -52,7 +40,7 @@ $error = get_transient(CC_MYWC_PLUGIN_SLUG . '_admin_attr_save_error');
             dir="<?php echo(is_rtl() ? 'rtl' : 'ltr') ?>" lang="<?php echo $locale ?>" required
             name="<?php echo CC_MYWC_PLUGIN_SLUG ?>-attributes[]">';
         <?php foreach ($attribute_taxonomies as $attribute_taxonomy) { ?>
-            <option value="<?php echo $attribute_taxonomy->attribute_id ?>"><?php echo $attribute_taxonomy->attribute_label ?></option>
+            <option <?php echo (in_array($attribute_taxonomy->attribute_id,$selected_data['attributes'])) ? 'selected="selected"' : '' ?> value="<?php echo $attribute_taxonomy->attribute_id ?>"><?php echo $attribute_taxonomy->attribute_label ?></option>
         <?php } ?>
     </select>
 
