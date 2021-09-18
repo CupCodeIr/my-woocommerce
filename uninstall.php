dd9db78fcca27096fc3408bf66c70ab98a30da83
Artin <info@cupcode.xyz>
2021-09-18 16:45:39 +0430

    Wiping logic and methods were updated.

--- uninstall.php
+++ uninstall.php
@@ -1,65 +1,83 @@
 <?php
+
+namespace CupCode\MyWooCommerce;
+
 defined('WP_UNINSTALL_PLUGIN') or die('No script kiddies please!');
 
 define('CC_MYWC_PLUGIN_SLUG', 'cupcode_mywc');
 define('CC_MYWC_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
 
-$cc_mywc_should_wipe = get_option(CC_MYWC_PLUGIN_SLUG . '_settings');
+$should_wipe = get_option(CC_MYWC_PLUGIN_SLUG . '_settings');
 
 /**
  * If user selected wipe, so wipe!
  */
-if ($cc_mywc_should_wipe['dc_all']) {
+if ($should_wipe['dc_all']) {
 
     delete_option(CC_MYWC_PLUGIN_SLUG . '_settings');
+    delete_option(CC_MYWC_PLUGIN_SLUG . '_attribute_endpoint');
+    wipe_selectable_attributes();
+    wipe_user_attributes();
 
 } else {
-    if ($cc_mywc_should_wipe['dc_settings'])
+    if ($should_wipe['dc_settings']){
+
         delete_option(CC_MYWC_PLUGIN_SLUG . '_settings');
-    if ($cc_mywc_should_wipe['dc_attributes']) {
-        //TODO
-//        cc_mywc_wipe_selectable_attributes();
+        delete_option(CC_MYWC_PLUGIN_SLUG . '_attribute_endpoint');
+
     }
-    //Maybe delete customer attributes
-    //Maybe delete endpoint option
+    if ($should_wipe['dc_attributes']) {
+
+        wipe_selectable_attributes();
+        wipe_user_attributes();
+    }
 
 }
 
 /**
  * Wipes all posts and related data such as postmeta and related taxonomy of '_sa' post type
+ * @since 0.1.0
  */
-function cc_mywc_wipe_selectable_attributes()
+function wipe_selectable_attributes()
 {
 
     global $wpdb;
-    $wpdb->query(
-        "DELETE p,pm,tr FROM {$wpdb->posts} p
-JOIN {$wpdb->postmeta} pm ON {$wpdb->posts}.id = {$wpdb->postmeta}.post_id
-JOIN {$wpdb->term_relationships} tr ON {$wpdb->posts}.id = {$wpdb->term_relationships}.object_id
-WHERE {$wpdb->posts}.post_type = '" . CC_MYWC_PLUGIN_SLUG . "_sa'"
-    );
+    $wpdb->query("DELETE p,pm
+FROM {$wpdb->posts} p
 
-    $update_taxonomies = ['product_tag', 'product_cat'];
-    foreach ($update_taxonomies as $update_taxonomy) {
-        $get_terms_args = array(
-            'taxonomy' => $update_taxonomy,
-            'fields' => 'ids',
-            'hide_empty' => false,
-        );
+         LEFT JOIN {$wpdb->postmeta} pm
+                   ON (p.ID = pm.post_id)
+WHERE p.post_type = " . CC_MYWC_PLUGIN_SLUG . "'_sa'");
 
-        $update_terms = get_terms($get_terms_args);
-        wp_update_term_count_now($update_terms, $update_taxonomy);
-    }
+
+}
+
+/**
+ * Wipes all posts and related data such as postmeta and related taxonomy of '_ua' post type
+ * @since 0.1.0
+ */
+function wipe_user_attributes()
+{
+
+    global $wpdb;
+    $wpdb->query("DELETE p,pm
+FROM {$wpdb->posts} p
+
+         LEFT JOIN {$wpdb->postmeta} pm
+                   ON (p.ID = pm.post_id)
+WHERE p.post_type = " . CC_MYWC_PLUGIN_SLUG . "'_ua'");
+
 
 }
 
 /**
  * Delete translation files
+ * @since 0.1.0
  */
 
 
-function cc_mywc_remove_translation_files()
+function remove_translation_files()
 {
 
     $mo_files = glob(CC_MYWC_PLUGIN_PATH . '/languages/*.mo');
@@ -69,5 +87,5 @@ function cc_mywc_remove_translation_files()
 
 }
 
-cc_mywc_remove_translation_files();
+remove_translation_files();
 
